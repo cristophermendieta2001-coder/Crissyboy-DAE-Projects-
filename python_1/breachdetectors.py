83ac83d81bf0df246d1e7c9e5fb8cc7af4273f84
cristophermendieta2001-coder <cristophermendieta2001@gmail.com>
2025-11-13 15:29:46 -0500

    What are you pushing

--- python_1/breachdetectors.py
+++ python_1/breachdetectors.py
@@ -10,27 +10,21 @@ def calculate_sha256(file_path):
             if not data:
                 break
             sha256_hash.update(data)
-        return sha256_hash.hexdigest()
+    return sha256_hash.hexdigest()
 
 
 def check_integrity(directory_path):
-    if not os.path.exist(directory_path) or not os.path.isdir(directory_path):
+    if not os.path.exists(directory_path) or not os.path.isdir(directory_path):
         print(f"Directory '{directory_path}' does not exist.")
         return
     
     for root, dirs, files in os.walk(directory_path):
         for file_name in files:
-            file_path= os.path.join(root, file_name)
+            file_path = os.path.join(root, file_name)
             calculated_hash = calculate_sha256(file_path)
-            print(f"File: {file_path}\nSHA-256 Hash {calculated_hash}")
+            print(f"File: {file_path}\nSHA-256 Hash: {calculated_hash}")
 
 
-        if __name__ == "__main__":
-            directory_to_check = input("Enter the directory path to check integrity: ")
-            check_integrity(directory_to_check)                
-            
-           
-
-                                                              
-
-    
+if __name__ == "__main__":
+    directory_to_check = input("Enter the directory path to check integrity: ")
+    check_integrity(directory_to_check)
